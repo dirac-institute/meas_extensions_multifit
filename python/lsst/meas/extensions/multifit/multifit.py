21899d8100162d925eeb29d28dac0a6b14acf278
Petar Zecevic <petar.zecevic@protonmail.com>
2023-11-03 15:33:56 +0000

    Multifit implementation

--- python/lsst/meas/extensions/multifit/multifit.py
+++ python/lsst/meas/extensions/multifit/multifit.py
@@ -1,24 +1,20 @@
-import numpy
-import logging
-import pickle
-from os.path import exists
+from .multifit_lib import sort_and_prepare_cutouts, generate_random_params, do_multifit
+from .multifit_lib import create_output_schema, lnlike_fun, calculate_t0
 
-import lsst.pex.config as pexConfig
-import lsst.daf.persistence as dafPersist
-import lsst.afw.image as afwImage
-import lsst.coadd.utils as coaddUtils
+import logging
+import numpy as np
+
 import lsst.pipe.base as pipeBase
 import lsst.pipe.base.connectionTypes as connectionTypes
-import lsst.pipe.tasks as pipeTasks
-from lsst.pipe.tasks.makeCoaddTempExp import MakeCoaddTempExpConfig, MakeWarpConnections, MakeWarpTask
-import lsst.utils as utils
+import lsst.afw.table as afwTable
+from lsst.obs.base import ExposureIdInfo
+from lsst.geom import SpherePoint, radians
+
+from lsst.pipe.tasks.makeCoaddTempExp import MakeCoaddTempExpConfig, MakeWarpTask
+
 import lsst.geom
-from lsst.meas.algorithms import CoaddPsf, CoaddPsfConfig
+
 from lsst.skymap import BaseSkyMap
-# from lsst.utils.timer import timeMethod
-# from .coaddBase import CoaddBaseTask
-from lsst.pipe.tasks.coaddBase import CoaddBaseTask, makeSkyInfo
-from collections.abc import Iterable
 
 __all__ = ["MultifitConnections", "MultifitTask", "MultifitConfig"]
 
@@ -30,10 +26,12 @@ __all__ = ["MultifitConnections", "MultifitTask", "MultifitConfig"]
 ## \ref MultifitTask "MultifitTask"
 ##      Task doing Multifit.
 ## \}
+
 log = logging.getLogger(__name__)
 
+
 class MultifitConnections(pipeBase.PipelineTaskConnections,
-                          dimensions=("tract", "patch", "skymap", "instrument", "band"),  #, "visit"),
+                          dimensions=("tract", "patch", "skymap", "instrument", "band"),  # , "visit"),
                           defaultTemplates={"coaddName": "deep",
                                             "skyWcsName": "jointcal",
                                             "photoCalibName": "fgcm",
@@ -42,7 +40,7 @@ class MultifitConnections(pipeBase.PipelineTaskConnections,
         doc="Input exposures to be resampled and optionally PSF-matched onto a SkyMap projection/patch",
         name="{calexpType}calexp",
         storageClass="ExposureF",
-        dimensions=("instrument", "visit", "detector"),
+        dimensions=("instrument", "visit", "detector", "band"),
         multiple=True,
         deferLoad=True,
     )
@@ -50,14 +48,14 @@ class MultifitConnections(pipeBase.PipelineTaskConnections,
         doc="Input backgrounds to be added back into the calexp if bgSubtracted=False",
         name="calexpBackground",
         storageClass="Background",
-        dimensions=("instrument", "visit", "detector"),
+        dimensions=("instrument", "visit", "detector", "band"),
         multiple=True,
     )
     skyCorrList = connectionTypes.Input(
         doc="Input Sky Correction to be subtracted from the calexp if doApplySkyCorr=True",
         name="skyCorr",
         storageClass="Background",
-        dimensions=("instrument", "visit", "detector"),
+        dimensions=("instrument", "visit", "detector", "band"),
         multiple=True,
     )
     skyMap = connectionTypes.Input(
@@ -96,27 +94,6 @@ class MultifitConnections(pipeBase.PipelineTaskConnections,
         storageClass="ExposureCatalog",
         dimensions=("instrument", "visit"),
     )
-    # wcsList = connectionTypes.Input(
-    #     doc="WCSs of calexps used by SelectImages subtask to determine if the calexp overlaps the patch",
-    #     name="{calexpType}calexp.wcs",
-    #     storageClass="Wcs",
-    #     dimensions=("instrument", "visit", "detector"),
-    #     multiple=True,
-    # )
-    # bboxList = connectionTypes.Input(
-    #     doc="BBoxes of calexps used by SelectImages subtask to determine if the calexp overlaps the patch",
-    #     name="{calexpType}calexp.bbox",
-    #     storageClass="Box2I",
-    #     dimensions=("instrument", "visit", "detector"),
-    #     multiple=True,
-    # )
-    # visitSummary = connectionTypes.Input(
-    #     doc="Consolidated exposure metadata from ConsolidateVisitSummaryTask",
-    #     name="{calexpType}visitSummary",
-    #     storageClass="ExposureCatalog",
-    #     dimensions=("instrument", "visit",),
-    #     multiple=True,
-    # )
     srcList = connectionTypes.Input(
         doc="Source catalogs used by PsfWcsSelectImages subtask to further select on PSF stability",
         name="src",
@@ -130,9 +107,14 @@ class MultifitConnections(pipeBase.PipelineTaskConnections,
         storageClass="SourceCatalog",
         dimensions=["band", "skymap", "tract", "patch"],
     )
+    multifitCat = pipeBase.connectionTypes.Output(
+        doc="Multifit measurement output catalog.",
+        name="meas_multifit",
+        storageClass="SourceCatalog",  # "DataFrame",
+        dimensions=["band", "skymap", "tract", "patch"],
+    )
 
-    def __init__(self, *, config=None):  # **kwargs):
-        # super().__init__(**kwargs)
+    def __init__(self, *, config=None):
         super().__init__(config=config)
         if config.bgSubtracted:
             self.inputs.remove("backgroundList")
@@ -156,142 +138,180 @@ class MultifitConnections(pipeBase.PipelineTaskConnections,
             self.inputs.remove("externalPhotoCalibGlobalCatalog")
         if not config.makeDirect:
             self.outputs.remove("direct")
-        # if not config.makePsfMatched:
-        #     self.outputs.remove("psfMatched")
-        # TODO DM-28769: add connection per selectImages connections
-        # if config.select.target != lsst.pipe.tasks.selectImages.PsfWcsSelectImagesTask:
-        #     self.inputs.remove("visitSummary")
-        #     self.inputs.remove("srcList")
-        # elif not config.select.doLegacyStarSelectionComputation:
-        #     # Remove backwards-compatibility connections.
-        #     self.inputs.remove("srcList")
+
+
 class MultifitConfig(pipeBase.PipelineTaskConfig, MakeCoaddTempExpConfig,
                      pipelineConnections=MultifitConnections):
-    outFile = pexConfig.Field(
-        doc="cutouts info output filename",
-        dtype=str,
-        default="/home/lsst/mnt/lsst-pkgs/rc2_subset/multifit_output.pickle",
-    )
     def validate(self):
         super().validate()
 
-class MultifitTask(MakeWarpTask):
 
+class MultifitTask(MakeWarpTask):
     ConfigClass = MultifitConfig
     _DefaultName = "multifit"
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self.schema = create_output_schema()
+        # self.outputSchema = afwTable.SourceCatalog(self.schema)
+        # self.outputSchema.getTable().setMetadata(self.algMetadata)
+
     def runQuantum(self, butlerQC, inputRefs, outputRefs):
         log.info("Starting Multifit runQuantum")
-        log.info(f"Here are my input refs: {inputRefs}")
-        log.info(f"Here are my output refs: {outputRefs}")
+        # log.info(f"Here are my input refs: {inputRefs}")
+        # log.info(f"Here are my output refs: {outputRefs}")
 
         inputs = butlerQC.get(inputRefs)
-        # for key in inputs:
-        #     if isinstance(inputs[key], list):
-        #         log.info(f"input {key}: {len(inputs[key])}")
-        # Construct skyInfo expected by `run`.  We remove the SkyMap itself
-        # from the dictionary so we can pass it as kwargs later.
-        # skyMap = inputs.pop("skyMap")
-        # quantumDataId = butlerQC.quantum.dataId
-        # skyInfo = makeSkyInfo(skyMap, tractId=quantumDataId['tract'], patchId=quantumDataId['patch'])
+
         # Construct list of input DataIds expected by `run`
         dataIdList = [ref.datasetRef.dataId for ref in inputRefs.calExpList]
-        # Construct list of packed integer IDs expected by `run`
-        # ccdIdList = [dataId.pack("visit_detector") for dataId in dataIdList]
-        # Run the selector and filter out calexps that were not selected
-        # primarily because they do not overlap the patch
-        # cornerPosList = lsst.geom.Box2D(skyInfo.bbox).getCorners()
-        # coordList = [skyInfo.wcs.pixelToSky(pos) for pos in cornerPosList]
-        # TODO: enable select task to speed up loading:
-        # goodIndices = self.select.run(**inputs, coordList=coordList, dataIds=dataIdList)
-        # log.info(f"coordList: {len(coordList)}")
-        # log.info(f"dataIdList: {len(dataIdList)}")
-        # log.info(f"goodind: {len(goodIndices)}")
-        # log.info(f"inputs: {len(inputs)}")
-        # inputs = self.filterInputs(indices=goodIndices, inputs=inputs)
+
         # Read from disk only the selected calexps
         inputs['calExpList'] = [ref.get() for ref in inputs['calExpList']]
 
-        # log.info(f"measCat input: {inputs['measCat']}")
-        self.run(inputs['calExpList'], inputs['measCat'].asAstropy(), dataIdList)  # , None, 0, None)
-    # @timeMethod
-    def run(self, calExpList, measCat, dataIdList):  # ccdIdList, skyInfo, visitId=0, dataIdList=None, **kwargs):
-        # log.info("Starting Multifit run")
-        EXT_PIXELS = 100
-        # deblend_modelType == MultiExtendedSource
-        extended_sources = measCat[measCat['deblend_modelType'] == 'MultiExtendedSource']
+        patch = inputRefs.measCat.dataId['patch']
 
-        all_cutouts = []
+        log.info(f"butler quantum dataId: {butlerQC.quantum.dataId}")
+
+        expId, expBits = butlerQC.quantum.dataId.pack("tract_patch", returnMaxBits=True)
+        idFactory = ExposureIdInfo(expId, expBits).makeSourceIdFactory()
+
+        outputs = self.run(inputs['calExpList'], inputs['measCat'].asAstropy(), dataIdList, patch, idFactory)  # , None, 0, None)
+        butlerQC.put(outputs, outputRefs)
+
+    # @timeMethod
+    def run(self, calExpList, measCat, dataIdList, patch, idFactory):  # ccdIdList, skyInfo, visitId=0, dataIdList=None, **kwargs):
+        # log.info("Starting Multifit run")
+
+        EXT_PIXELS = 21
+
+        extended_sources = measCat  # [measCat['deblend_modelType'] == 'MultiExtendedSource']
+
+        if len(extended_sources) > 0:
+            log.info(f"Multifit measuring {len(extended_sources)} extended sources")
+
+        table = afwTable.SourceTable.make(self.schema, idFactory)
+        catalog = afwTable.SourceCatalog(table)
+        catalog.reserve(len(extended_sources))
 
         for source in extended_sources:
-            ra = source['coord_ra']
-            dec = source['coord_dec']
+            ra = source['coord_ra'] * 180 / np.pi
+            dec = source['coord_dec'] * 180 / np.pi
 
-            coord = lsst.geom.SpherePoint(ra, dec, lsst.geom.radians)
+            coord = lsst.geom.SpherePoint(ra, dec, lsst.geom.degrees)
 
+            clones = []
             cutouts = []
+            dids = []
             wcss = []
             psfs = []
             xs = []
             ys = []
             times = []
             offsets = []
+            variances = []
+            masks = []
+            calibMeans = []
+
+            srcid = source['id']
 
             for i, calexp in enumerate(calExpList):
                 dataId = dataIdList[i]
                 wcs = calexp.getWcs()
-                psf = calexp.getPsf().computeImage()
                 sourcexy = wcs.skyToPixel(coord)
+                psf = calexp.getPsf().computeImage(sourcexy)
+                # psf = calexp.getPsf(sourcexy).computeImage()
 
-                # log.info(f"src in calexp: {sourcexy}")
                 bb = calexp.getBBox()
 
                 if bb.beginX + EXT_PIXELS/2 > sourcexy.x or bb.beginY + EXT_PIXELS/2 > sourcexy.y or \
-                    bb.endX - EXT_PIXELS/2 < sourcexy.x or bb.endY - EXT_PIXELS/2 < sourcexy.y:
-                    # log.info(f"Source not contained in calexp")
+                   bb.endX - EXT_PIXELS/2 < sourcexy.x or bb.endY - EXT_PIXELS/2 < sourcexy.y:
                     continue
-                # img = calexp.image.getArray()
+
                 extent = lsst.geom.ExtentI(EXT_PIXELS, EXT_PIXELS)
                 cutout = calexp.getCutout(coord, extent)
-                image_arr = cutout.getMaskedImage().getImage().array
 
-                offset = (cutout.getBBox().getCorners()[0], cutout.getBBox().getCorners()[2], wcs.skyToPixel(coord))
+                image_arr = cutout.getImage().array
+                # image_arr = cutout.getMaskedImage().getImage().array
 
+                offset = (cutout.getBBox().getCorners()[0], cutout.getBBox().getCorners()[2])
+                bx = cutout.getBBox().getBeginX()
+                by = cutout.getBBox().getBeginY()
+                mask = calexp.getMask().getArray()[by:by + EXT_PIXELS, bx:bx + EXT_PIXELS]
+                variance = calexp.getVariance().getArray()[by:by + EXT_PIXELS, bx:bx + EXT_PIXELS]
+                calibMean = calexp.getPhotoCalib().getCalibrationMean()
+
+                clones.append(calexp.clone())
                 cutouts.append(image_arr)
+                dids.append(dataId)
                 wcss.append(wcs)
                 psfs.append(psf)
                 xs.append(sourcexy.x)
                 ys.append(sourcexy.y)
                 times.append(calexp.visitInfo.date)
                 offsets.append(offset)
+                variances.append(variance)
+                masks.append(mask)
+                calibMeans.append(calibMean)
 
-            if len(cutouts) > 4:
-                # log.info(f"Found {len(cutouts)} cutouts for source ({ra}, {dec})")
-                cutouts_info = {}
-                cutouts_info['dataId'] = dataId
-                cutouts_info['id'] = source['id']
-                cutouts_info['base_GaussianFlux_instFlux'] = source['base_GaussianFlux_instFlux']
-                cutouts_info['base_PsfFlux_instFlux'] = source['base_PsfFlux_instFlux']
-                cutouts_info['modelfit_CModel_initial_instFlux'] = source['modelfit_CModel_initial_instFlux']
-                cutouts_info['ra'] = ra
-                cutouts_info['dec'] = dec
-                cutouts_info['xs'] = xs
-                cutouts_info['ys'] = ys
-                cutouts_info['times'] = times
-                cutouts_info['cutouts'] = cutouts
-                cutouts_info['wcss'] = wcss
-                cutouts_info['psfs'] = psfs
-                cutouts_info['offsets'] = offsets
+            if len(cutouts) == 0:
+                log.warn(f"No calexps of extended source {srcid}")
+                continue
 
-                all_cutouts.append(cutouts_info)
-            # break
-        output = []
-        if exists(self.config.outFile):
-            with open(self.config.outFile, 'rb') as f:
-                output = pickle.load(f)
-        output.append(all_cutouts)
+            cutouts_info = {}
+            cutouts_info['dataIds'] = dids
+            cutouts_info['id'] = srcid
+            cutouts_info['ra'] = ra
+            cutouts_info['dec'] = dec
+            cutouts_info['xs'] = xs
+            cutouts_info['ys'] = ys
+            cutouts_info['times'] = times
+            cutouts_info['cutouts'] = cutouts
+            cutouts_info['wcss'] = wcss
+            cutouts_info['psfs'] = psfs
+            cutouts_info['offsets'] = offsets
+            cutouts_info['masks'] = masks
+            cutouts_info['variances'] = variances
+            cutouts_info['calibMeans'] = calibMeans
 
-        with open(self.config.outFile, 'wb') as f:
-            pickle.dump(output, f)
+            cutouts_info = sort_and_prepare_cutouts(cutouts_info, cutout_size=EXT_PIXELS)
+
+            start_params = generate_random_params(1, mag_rrange=6, mag_roffs=19,
+                                                  pos0_rrange=1/10, pos0_roffs=-1/20,
+                                                  v_rrange=1/100, v_roffs=-1/200)[0]
+
+            mr = do_multifit(cutouts_info, None, None, in_collection=None,
+                             start_params=start_params, clones=clones, do_normalize=False,
+                             method='powell', verbose=0)
+            mag, x0, y0, vx, vy = mr.x
+            mag_err, x0_err, y0_err, vx_err, vy_err = mr.errors
+
+            lnlikeraw = lnlike_fun(mr.x, cutouts_info, clones)
+            chisq = np.sum(lnlikeraw ** 2)
+            chisqperdof = chisq / len(lnlikeraw)
+
+            t0 = calculate_t0(cutouts_info)
+
+            row = catalog.addNew()
+            row.setParent(srcid)
+            radec = SpherePoint(source['coord_ra'], source['coord_dec'], radians)
+            row.setRa(radec.getRa())
+            row.setDec(radec.getDec())
+            row.set("ra0", ra+x0/3600)
+            row.set("dec0", dec+y0/3600)
+            row.set("mag", mag)
+            row.set("t0", t0)
+            row.set("x0", x0)
+            row.set("y0", y0)
+            row.set("vx", vx)
+            row.set("vy", vy)
+            row.set("mag_err", mag_err)
+            row.set("x0_err", x0_err)
+            row.set("y0_err", y0_err)
+            row.set("vx_err", vx_err)
+            row.set("vy_err", vy_err)
+            row.set("chi_sq", chisq)
+            row.set("chi_sq_per_dof", chisqperdof)
+
+        return pipeBase.Struct(multifitCat=catalog)
